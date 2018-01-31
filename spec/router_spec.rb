fff6c3042dc0a7844e421f4501899aa2ff098c6d
Joe Sak <joe@joesak.com>
2018-01-31 16:30:29 -0600

    Improve router spec

--- spec/router_spec.rb
+++ spec/router_spec.rb
@@ -26,15 +26,8 @@ RSpec.describe "Routes" do
 
   describe "GET /" do
     it "returns all recurring bills" do
-      DB.connection.exec("
-        insert into recurring_bills (name, amount_in_pennies, pay_period)
-        values('credit cards', 50000, 1);
-      ")
-
-      DB.connection.exec("
-        insert into recurring_bills (name, amount_in_pennies, pay_period)
-        values('savings', 25000, 1);
-      ")
+      DB.create_recurring_bill("credit cards", 50_000, 1)
+      DB.create_recurring_bill("savings", 25_000, 1)
 
       get("/")
 
@@ -43,7 +36,9 @@ RSpec.describe "Routes" do
         "savings",
       ]
 
-      JSON.parse(last_response.body).map { |j| j['amount_in_pennies'] }.should == [
+      JSON.parse(last_response.body).map { |j|
+        j['amount_in_pennies']
+      }.should == [
         "50000",
         "25000",
       ]
@@ -59,7 +54,19 @@ RSpec.describe "Routes" do
       }),
       { 'CONTENT_TYPE' => 'application/json' })
 
-      JSON.parse(last_response.body)["id"].should_not be_nil
+      DB.last_recurring_bill.id.should_not be_nil
+    end
+
+    it "returns the created recurring bill" do
+      post("/", JSON.generate({
+        name: "car payment",
+        amount_in_pennies: 36800,
+        pay_period: 2,
+      }),
+      { 'CONTENT_TYPE' => 'application/json' })
+
+      JSON.parse(last_response.body)["id"].should ==
+        DB.last_recurring_bill.id
     end
   end
 end
